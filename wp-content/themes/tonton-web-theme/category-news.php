f7c0ecdc19382ec2b1f340b5a442b4d562a9d562
yoko <hatakeyama.works216@gmail.com>
2022-09-13 20:15:43 +0900

    [news]検索追加etc.

--- wp-content/themes/tonton-web-theme/category-news.php
+++ wp-content/themes/tonton-web-theme/category-news.php
@@ -32,55 +32,132 @@ get_header();
       </div>
       <div class="news-content wrapper">
         <ul class="breadcrumbs">
-          <li><a href="http://webliker.info">TOP</a></li>
+          <li><a href="/">TOP</a></li>
           <li>お知らせ一覧</li>
         </ul>
-        <div class="news-tab-top dis-pc">
-          <ul>
-            <li class="news-select">すべて</li>
-            <li>ニュース</li>
-            <li>新規</li>
-            <li>成約</li>
-            <li>価格変更</li>
-          </ul>
-        </div>
-        <div class="news-tab-top dis-sp">
-          <ul>
-            <li class="news-select">すべて</li>
-            <li>ニュース</li>
-            <li>新規</li>
-          </ul>
-          <ul>
-            <li>成約</li>
-            <li>価格変更</li>
-          </ul>
-        </div>
-        <div class="news-tab-bottom">
-          <ul>
-            <li class="news-select">すべて</li>
-            <li>2022</li>
-            <li>2021</li>
-          </ul>
-        </div>
+
         <div class="news-wrapper">
+          <!-- 1. 検索条件の取得と変数の設定 -->
+          <?php
+            // ページ番号を取得
+            $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+            $args = array(
+              'paged' => $paged, // ページ番号を設定
+              'post_type' => 'post',
+              'post_status' => 'publish',
+              'posts_per_page' => 5, // 一つのページに表示する件数を設定
+              'orderby' => 'date',
+              'order' => 'DESC'
+            );
+
+            // POSTをGETに変更
+            if(!empty($_GET['search_category'])) {
+              foreach($_GET['search_category'] as $value) {
+                if ($value === 'all') {
+                  $posts = get_categories('post', Array('hide_empty' => false));
+                  foreach($posts as $post):
+                    $search_category[] = htmlspecialchars($post->term_id, ENT_QUOTES);
+                  endforeach;
+                } else {
+                  $search_category[] = htmlspecialchars($value, ENT_QUOTES);
+                }
+              }
+              $args += array('category__in' => $search_category);
+            }
+
+            if(!empty($_GET['search_tag'])) {
+              foreach($_GET['search_tag'] as $value) {
+                if ($value === 'tag-all') {
+                  $tags_posts = get_tags('post', Array('hide_empty' => false));
+                  foreach($tags_posts as $tags_post):
+                    $search_tag[] = htmlspecialchars($tags_post->term_id, ENT_QUOTES);
+                  endforeach;
+                } else {
+                  $search_tag[] = htmlspecialchars($value, ENT_QUOTES);
+                } 
+              }
+              $args += array('tag__in' => $search_tag);
+            }
+          ?>
+
+          <!-- 2. 検索フォームの表示 -->
+          <form method="get" action="/news/">
+            <div class="news-tab-top">
+              <ul>
+                <li>
+                  <label for="all">
+                    <input type="radio" id="all" name="search_category[]" value="all" onclick="submit();" checked />
+                    <span>すべて</span>
+                  </label>
+                </li>
+                <?php
+                  $categories = get_categories(Array('hide_empty' => false));
+                  foreach($categories as $category):
+                    $checked = "";
+                    if ($_GET['search_category'][0] === 'all') {
+                      $checked = "";
+                    } else {
+                      if(in_array($category->term_id, (array)$search_category)) $checked = " checked";
+                    }
+                ?>
+                  <li>
+                    <label>
+                      <input type="radio" name="search_category[]" value="<?php echo esc_attr($category->term_id); ?>"<?php echo $checked; ?> onclick="submit();">
+                      <span><?php echo esc_html($category->name); ?></span>
+                    </label>
+                  </li>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+                
+            <div class="news-tab-bottom">
+              <ul>
+                <li>
+                  <label for="tag-all">
+                    <input type="radio" id="tag-all" name="search_tag[]" value="tag-all" onclick="submit();" checked />
+                    <span>すべて</span>
+                  </label>
+                </li>
+                <?php
+                  $tags = get_tags(Array('hide_empty' => false));
+                  foreach($tags as $tag):
+                    $checked = "";
+                    if ($_GET['search_tag'][0] === 'tag-all') {
+                      $checked = "";
+                    } else {
+                      if(in_array($tag->term_id, (array)$search_tag)) $checked = " checked";
+                    }
+                ?>
+                  <li>
+                    <label>
+                      <input type="radio" name="search_tag[]" value="<?php echo esc_attr($tag->term_id); ?>"<?php echo $checked; ?> onclick="submit();">
+                      <span><?php echo esc_html($tag->name); ?></span>
+                    </label>
+                  </li>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+          </form>
+                                                                      
+          <!-- 3. 検索結果の取得と表示 -->
           <div class="news-list">
             <ul>
               <?php
-                $args = array(
-                  'posts_per_page' => 10 // 表示件数の指定
-                );
-                $posts = get_posts( $args );
-                foreach ( $posts as $post ): // ループの開始
-                setup_postdata( $post ); // 記事データの取得
+                $the_query = new WP_Query($args);
+                if($the_query->have_posts()) :
+              ?>	
+              <?php
+                while($the_query->have_posts()) :
+                  $the_query->the_post();
               ?>
                 <li>
                   <a href="<?php the_permalink(); ?>">
                     <div class="news-item-box">
                       <div class="news-item">
                         <div class="news-date"><?php the_time("Y.n.j"); ?></div>
-                        <div class="news-tags news-tags-<?php foreach((get_the_tags()) as $tag) { echo $tag->slug . ' ';} ?>">
-                          <?php foreach((get_the_tags()) as $tag) {
-                            echo $tag->name . ' ';
+                        <div class="news-tags news-tags-<?php foreach((get_the_category()) as $category) { echo $category->slug . ' ';} ?>">
+                          <?php foreach((get_the_category()) as $tag) {
+                            echo $category->name . ' ';
                           } ?>
                         </div>
                       </div>
@@ -90,44 +167,47 @@ get_header();
                     </div>
                   </a>
                 </li>
-              <?php
-                endforeach; // ループの終了
-                wp_reset_postdata(); // 直前のクエリを復元する
-              ?>
+              <?php endwhile; wp_reset_postdata(); ?>
+              <!-- 3. 検索結果の取得と表示 -->
+              <nav class="pagination">
+                <div class="nav-links">
+                  <?php
+                    // ページャーを設置
+                    $big = 99999;
+                    echo paginate_links(array(
+                      'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                      'total' => $the_query->max_num_pages,
+                      'current' => $paged,
+                      'prev_text' => '&lsaquo;',
+                      'next_text' => '&rsaquo;',
+                      'type' => 'list',
+                    )); 
+                  ?>
+                </div>
+              </nav>
+              <?php else : ?>
+                <div class="news-item-none">
+                  <p>該当する物件はありませんでした。</p>
+                </div>
+              <?php endif; ?>
+
+              </div>
             </ul>
           </div>
-          <nav class="pagination">
-            <div class="nav-links">
-              <a class="prev" href="">&lsaquo;</a>
-              <span aria-current="page" class="page-numbers current">1</span>
-              <a class="page-numbers" href="">2</a>
-              <a class="page-numbers" href="">3</a>
-              <a class="page-numbers" href="">4</a>
-              <a class="page-numbers" href="">5</a>
-              <a class="page-numbers pc" href="">6</a>
-              <a class="page-numbers pc" href="">7</a>
-              <a class="page-numbers pc" href="">8</a>
-              <a class="page-numbers pc" href="">9</a>
-              <a class="page-numbers pc" href="">10</a>
-              <span class="page-numbers dots">…</span>
-              <a class="page-numbers" href="">62</a>
-              <a class="next" href="">&rsaquo;</a>
-            </div>
-          </nav>
-        </div>
-        <div class="news-button-wrapper">
-          <div class="top-button top-button--green">
-            <div class="top-buttonInner">
-              <a href="/">
-                TOPへ戻る
-              </a>
-              <div class="top-icon">
-                <img src="<?php echo get_template_directory_uri();?>/images/common/tonton-boy.png" alt="">
-              </div>
-              <div class="top-arrow">
-                <svg xmlns="http://www.w3.org/2000/svg" width="15.96" height="12.424" viewBox="0 0 15.96 12.424">
-                  <path d="M1427.751,843.521l-5.656-5.3c-.775-.727-1.932.455-1.153,1.183l4.225,3.957-12.349.011a.839.839,0,0,0,0,1.674l12.206-.01L1420.916,849a.827.827,0,0,0,1.154,1.184l5.681-5.482A.857.857,0,0,0,1427.751,843.521Z" transform="translate(-1412.029 -837.997)" fill="#fff"/>
-                </svg>
+          <div class="news-button-wrapper">
+            <div class="top-button top-button--green">
+              <div class="top-buttonInner">
+                <a href="/">
+                  TOPへ戻る
+                </a>
+                <div class="top-icon">
+                  <img src="<?php echo get_template_directory_uri();?>/images/common/tonton-boy.png" alt="">
+                </div>
+                <div class="top-arrow">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="15.96" height="12.424" viewBox="0 0 15.96 12.424">
+                    <path d="M1427.751,843.521l-5.656-5.3c-.775-.727-1.932.455-1.153,1.183l4.225,3.957-12.349.011a.839.839,0,0,0,0,1.674l12.206-.01L1420.916,849a.827.827,0,0,0,1.154,1.184l5.681-5.482A.857.857,0,0,0,1427.751,843.521Z" transform="translate(-1412.029 -837.997)" fill="#fff"/>
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
