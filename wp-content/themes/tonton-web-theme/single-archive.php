05ce1258746f5a8896aaa4840cd2abd5ae084ac8
yoko <hatakeyama.works216@gmail.com>
2022-09-13 23:54:07 +0900

    デザイン修正

--- wp-content/themes/tonton-web-theme/single-archive.php
+++ wp-content/themes/tonton-web-theme/single-archive.php
@@ -142,6 +142,11 @@ get_header();
               <a href="/business/reform/results-archive/">
                 <div class="archive-detail-button">
                   物件一覧に戻る
+                  <div class="archive-arrow">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15.96" height="12.424" viewBox="0 0 15.96 12.424">
+                      <path d="M3077.281,5093.885l5.655,5.3c.775.727,1.934-.455,1.155-1.184l-4.225-3.958,12.349-.01a.839.839,0,0,0,0-1.675l-12.206.011,4.106-3.963a.827.827,0,0,0-1.154-1.184l-5.68,5.482A.856.856,0,0,0,3077.281,5093.885Z" transform="translate(-3077.043 -5086.985)" fill="#009844"/>
+                    </svg>
+                  </div>
                 </div>
               </a>
             </div>
